8a9143c8c436c24ea6041843c32c050befad4e8f
Craig J <johnsonc@byui.edu>
2022-01-04 14:03:49 -0700

    Fleshing out Lesson 3 Script

--- lesson03_script.R
+++ lesson03_script.R
@@ -3,33 +3,67 @@
 # Sample R code
 # https://byuistats.github.io/BYUI_M221_Book_R
 
-# Read in tuberculosis data
-mydata <- c(15100, 19000, 4800, 6500, 14900, 600, 23500, 11500, 12900, 32200)
+# Read in tuberculosis data (Note: this is a vector)
+tuberc <- c(15100, 19000, 4800, 6500, 14900, 600, 23500, 11500, 12900, 32200)
 
-# -----------------------------------------
+# --------------------------------------------------
+# Numerical Summaries: Measures of center
+#                      Mean, Median, and Mode
+# --------------------------------------------------
+
+# Compute the mean
+mean(tuberc)
+
+# Compute the median
+median(tuberc)
+
+# Find the mode
+table(tuberc)
+
+
+# --------------------------------------------------
 # Histograms  
-# -----------------------------------------
+# --------------------------------------------------
 
 # Basic histogram of the tuberculosis data
-hist(mydata)
+hist(tuberc)
 
-# Nicer histogram of the tuberculosis data
-hist(mydata, 
+# Optional nicer histogram of the tuberculosis data
+hist(tuberc, 
      col = "skyblue", 
      main = "Histogram of Costs to Treat Tuberculosis", 
      xlab = "Cost in Dollars", 
      ylab = "Number of Individuals")
 
 
-# -----------------------------------------
-# Numerical Summaries: Measures of center
-# -----------------------------------------
+# --------------------------------------------------
+# Example of Reading Data from a Web Page
+# --------------------------------------------------
 
-# Compute the mean
-mean(mydata)
+# Read in required packages
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(curl, readxl)
 
-# Compute the median
-median(mydata)
+# To download an Excel file from the web, replace the URL and run this code:
+url <- "https://byuistats.github.io/BYUI_M221_Book_R/Data/wrong_site_wrong_patient.xlsx"
+destfile <- basename(url)
+curl::curl_download(url, destfile)
+mydata <- read_excel(destfile)
+View(mydata)
 
-# Find the mode
-table(mydata)
+# Compute the mean of the wrong_site data
+mean(mydata$wrong_site)
+
+# Compute the median of the wrong_site data
+median(mydata$wrong_site)
+
+# Find the mode of the wrong_site data 
+# (Note: this is not very helpful)
+table(mydata$wrong_site)
+
+# Find the mode of the wrong_site data (Sorted in ascending order)
+# Notice the two most-frequently occuring value is 0, with 50 instances
+sort(table(mydata$wrong_site))
+
+# Basic histogram of the wrong_site data
+hist(mydata$wrong_site)
