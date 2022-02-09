c42da123b4658325ac7bf31db4895cc88bf39c79
Craig J <johnsonc@byui.edu>
2022-02-08 17:29:48 -0700

    updated data files

--- scripts/lesson13.R
+++ scripts/lesson13.R
@@ -11,24 +11,48 @@ library(mosaic)
 
 
 
-# Long Data: One Column of Data, One Column Indicates the Group ===============
+# Hypothesis Tests and Confidence Intervals (Wide Data) =============
 
 
 
-# Hypothesis Tests ==================================================
+# COPD Example --------------------------------------------
+# Use "Import Dataset" to read the data into R
+# https://byuistats.github.io/M221R/data/copd_rehab.xlsx
+# (The "Import Dataset" code was pasted here:)
+library(readxl)
+url <- "https://byuistats.github.io/M221R/data/copd_rehab.xlsx"
+destfile <- "copd_rehab.xlsx"
+curl::curl_download(url, destfile)
+copd_rehab <- read_excel(destfile)
+View(copd_rehab)
+
+# Numerical Summaries -------------------------------------
+favstats(copd_rehab$community)
+favstats(copd_rehab$hospital)
+
+# Graphical Summaries -------------------------------------
+boxplot(copd_rehab$community, copd_rehab$hospital)
+
+# Hypothesis Test -----------------------------------------
+t.test(copd_rehab$community, copd_rehab$hospital,
+       mu = 0,  
+       alternative = "two.sided")
+
+# Confidence Interval -------------------------------------
+t.test(copd_rehab$community, copd_rehab$hospital, 
+       conf.level = 0.95)
+
+
+
+# Hypothesis Tests and Confidence Intervals (Long Data) =============
 
 
 
 # Reading Practices Example -------------------------------
 # Use "Import Dataset" to read the data into R
-# https://byuistats.github.io/M221R/data/reading.xlsx
-# (The "Import Dataset" code was pasted here:)
-library(readxl)
-url <- "https://byuistats.github.io/M221R/data/reading.xlsx"
-destfile <- "reading.xlsx"
-curl::curl_download(url, destfile)
-reading <- read_excel(destfile)
-View(reading)
+# https://byuistats.github.io/M221R/data/copd_rehab.xlsx
+# (You may want to paste the code here)
+
 
 # Numerical Summaries -------------------------------------
 favstats(reading$nights ~ reading$group)
@@ -39,12 +63,14 @@ boxplot(reading$nights ~ reading$group)
 # Hypothesis Test -----------------------------------------
 t.test(reading$nights ~ reading$group, 
        mu = 0, 
-       alternative = "two.sided", 
+       alternative = "two.sided")
+
+# Confidence Interval -------------------------------------
+t.test(reading$nights ~ reading$group, 
        conf.level = 0.95)
 
 
-
-# FIFA World Cup Heart Attack Data ------------------------
+# FIFA Heart Attacks Example ------------------------------
 # Use "Import Dataset" to read the data into R
 # https://byuistats.github.io/M221R/data/fifa_heart_attacks.xlsx
 # (You may want to paste the code here)
@@ -56,55 +82,11 @@ favstats(fifa_heart_attacks$heart_attacks ~ fifa_heart_attacks$time_period)
 # Graphical Summaries -------------------------------------
 boxplot(fifa_heart_attacks$heart_attacks ~ fifa_heart_attacks$time_period)
 
-# Hypothesis Test -----------------------------------------
+# Hypothesis Test (Wide Data) -----------------------------
 t.test(fifa_heart_attacks$heart_attacks ~ fifa_heart_attacks$time_period, 
        mu = 0,
-       alternative = "two.sided",
+       alternative = "two.sided")
+
+# Confidence Interval (Wide Data) -------------------------
+t.test(fifa_heart_attacks$heart_attacks ~ fifa_heart_attacks$time_period, 
        conf.level = 0.95)
-
-
-
-# Confidence Intervals ==============================================
-
-
-
-# Reading Practices Example -------------------------------
-# Use "Import Dataset" to read the data into R
-# https://byuistats.github.io/M221R/data/reading.xlsx
-# (You may want to paste the code here)
-
-
-# Numerical Summaries -------------------------------------
-favstats(reading$nights ~ reading$group)
-
-# Graphical Summaries -------------------------------------
-boxplot(reading$nights ~ reading$group)
-
-# Confidence Interval -------------------------------------
-t.test(reading$nights ~ reading$group, conf.level = 0.95)
-
-
-
-# Wide Data: Data Stored in Separate Columns ========================
-
-
-
-# COPD ----------------------------------------------------
-# Use "Import Dataset" to read the data into R
-# https://byuistats.github.io/M221R/data/copd_rehab.xlsx
-# (You may want to paste the code here)
-
-
-# Numerical Summaries -------------------------------------
-favstats(copd_rehab$community)
-favstats(copd_rehab$hospital)
-
-# Graphical Summaries -------------------------------------
-boxplot(copd_rehab$community, copd_rehab$hospital,
-        names = c("Community", "Hospital"))
-
-# Confidence Interval -------------------------------------
-t.test(copd_rehab$community, copd_rehab$hospital, 
-       mu = 0,
-       alternative = "two.sided",
-       conf.level = 0.90)
