93a9bf96d61e32734af3a5121f170b647b4d71d7
Craig J <johnsonc@byui.edu>
2022-02-23 18:38:45 -0700

    revised stack / filter commands

---
+++ docs/scripts/lesson15_next_semester.R
@@ -0,0 +1,256 @@
+# =============================================================================
+# Lesson 15: Unit 2 Review
+# Sample R code
+# MATH 221D
+# Brigham Young University-Idaho
+# =============================================================================
+
+
+
+# Lesson 3: Describing Quantitative Data (Shape & Center) =====================
+
+
+
+# Tuberculosis Example ------------------------------------
+# Use "Import Dataset" to read the data into R
+# https://byuistats.github.io/M221R/data/tuberculosis.xlsx
+# (The "Import Dataset" code was pasted here:)
+library(readxl)
+url <- "https://byuistats.github.io/M221R/data/tuberculosis.xlsx"
+destfile <- "tuberculosis.xlsx"
+curl::curl_download(url, destfile)
+tuberculosis <- read_excel(destfile)
+View(tuberculosis)
+
+# Numerical Summaries -------------------------------------
+library(mosaic)
+favstats(tuberculosis$costs)
+
+# Graphical Summaries -------------------------------------
+hist(tuberculosis$costs)
+
+
+
+# Lesson 4: Describing Quantitative Data (Spread) =============================
+
+
+
+# Surgery Lawsuits Example --------------------------------
+# Use "Import Dataset" to read the data into R
+# https://byuistats.github.io/M221R/data/surgery_lawsuits.xlsx
+# (The "Import Dataset" code was pasted here:)
+library(readxl)
+url <- "https://byuistats.github.io/M221R/data/surgery_lawsuits.xlsx"
+destfile <- "surgery_lawsuits.xlsx"
+curl::curl_download(url, destfile)
+surgery_lawsuits <- read_excel(destfile)
+View(surgery_lawsuits)
+
+# Numerical Summaries -------------------------------------
+library(mosaic)
+favstats(surgery_lawsuits$wrong_site)
+quantile(surgery_lawsuits$wrong_site, 0.25) # 25th percentile
+
+# Graphical Summaries -------------------------------------
+hist(surgery_lawsuits$wrong_site)
+boxplot(surgery_lawsuits$wrong_site, horizontal = TRUE)
+
+
+
+# Lesson 11: Inference for One Mean (Sigma Unknown) ===========================
+
+
+
+# Body Temperatures Example -------------------------------
+# Use "Import Dataset" to read the data into R
+# https://byuistats.github.io/M221R/data/body_temp.xlsx
+# (The "Import Dataset" code was pasted here:)
+library(readxl)
+url <- "https://byuistats.github.io/M221R/data/body_temp.xlsx"
+destfile <- "body_temp.xlsx"
+curl::curl_download(url, destfile)
+body_temp <- read_excel(destfile)
+View(body_temp)
+
+# Numerical Summaries -------------------------------------
+library(mosaic)
+favstats(body_temp$temperature) 
+
+# Graphical Summaries -------------------------------------
+hist(body_temp$temperature)
+boxplot(body_temp$temperature, horizontal=TRUE)
+
+# Hypothesis Test -----------------------------------------
+t.test(body_temp$temperature,      # Data
+       mu = 98.6,                  # Value from null hypothesis
+       alternative = "two.sided")  # Could be "two.sided", "greater" or "less"
+
+# Confidence Interval -------------------------------------
+t.test(body_temp$temperature,      # Data
+       conf.level = 0.95)          # Confidence level
+
+
+
+# Lesson 12: Inference for the Mean of Differences (Two Dependent Samples) ====
+
+
+
+# Weight Loss Example -------------------------------
+# Use "Import Dataset" to read the data into R
+# https://byuistats.github.io/M221R/data/weight_loss.xlsx
+# (The "Import Dataset" code was pasted here:)
+library(readxl)
+url <- "https://byuistats.github.io/M221R/data/weight_loss.xlsx"
+destfile <- "weight_loss.xlsx"
+curl::curl_download(url, destfile)
+weight_loss <- read_excel(destfile)
+View(weight_loss)
+
+# Compute Column of Differences
+weight_loss$difference <- weight_loss$post - weight_loss$pre
+
+# Numerical Summaries -------------------------------------
+library(mosaic)
+favstats(weight_loss$difference) 
+
+# Graphical Summaries -------------------------------------
+hist(weight_loss$difference)
+boxplot(weight_loss$difference, horizontal=TRUE)
+
+# Hypothesis Test -----------------------------------------
+t.test(weight_loss$difference,     # Data
+       mu = 0,                     # Value from null hypothesis
+       alternative = "two.sided",  # Could be "two.sided", "greater" or "less"
+       conf.level = 0.95)          # Confidence level for a confidence interval
+
+# Confidence Interval -------------------------------------
+t.test(weight_loss$difference,     # Data
+       conf.level=0.95)            # Confidence level
+
+
+
+# Lesson 13: Inference for the Difference of Means (Two Independent Samples) ==
+
+
+
+# FIFA Heart Attacks Example (Long Data) ------------------
+# Use "Import Dataset" to read the data into R
+# https://byuistats.github.io/M221R/data/fifa_heart_attacks.xlsx
+# (The "Import Dataset" code was pasted here:)
+library(readxl)
+url <- "https://byuistats.github.io/M221R/data/fifa_heart_attacks.xlsx"
+destfile <- "fifa_heart_attacks.xlsx"
+curl::curl_download(url, destfile)
+fifa_heart_attacks <- read_excel(destfile)
+View(fifa_heart_attacks)
+
+# Numerical Summaries -------------------------------------
+library(mosaic)
+favstats(heart_attacks ~ time_period, data = fifa_heart_attacks)
+
+# Graphical Summaries -------------------------------------
+boxplot(heart_attacks ~ time_period, data = fifa_heart_attacks)
+
+# Hypothesis Test -----------------------------------------
+t.test(heart_attacks ~ time_period, data = fifa_heart_attacks,
+       alternative = "less")
+
+# Confidence Interval -------------------------------------
+t.test(heart_attacks ~ time_period, data = fifa_heart_attacks,
+       conf.level = 0.95)
+
+
+
+# COPD Example (Wide Data) --------------------------------
+# Use "Import Dataset" to read the data into R
+# https://byuistats.github.io/M221R/data/copd_rehab.xlsx
+# (The "Import Dataset" code was pasted here:)
+library(readxl)
+url <- "https://byuistats.github.io/M221R/data/copd_rehab.xlsx"
+destfile <- "copd_rehab.xlsx"
+curl::curl_download(url, destfile)
+copd_rehab <- read_excel(destfile)
+View(copd_rehab)
+
+# Convert Data from Wide to Long Format (if necessary) ----
+copd <- stack( Filter(is.numeric, copd_rehab) )
+
+# Numerical Summaries -------------------------------------
+library(mosaic)
+favstats(values ~ ind, data = copd)
+
+# Graphical Summaries -------------------------------------
+boxplot(values ~ ind, data = copd)
+
+# Hypothesis Test -----------------------------------------
+t.test(values ~ ind, data = copd,
+       alternative = "two.sided")
+
+# Confidence Interval -------------------------------------
+t.test(values ~ ind, data = copd,
+       conf.level = 0.95)
+
+
+
+# Lesson 14: Inference using ANOVA (Several Independent Samples) ==============
+
+
+
+# Gratitude Example (Long Data) ---------------------------
+# Use "Import Dataset" to read the data into R
+# https://byuistats.github.io/M221R/data/gratitude.xlsx
+# (The "Import Dataset" code was pasted here:)
+library(readxl)
+url <- "https://byuistats.github.io/M221R/data/gratitude.xlsx"
+destfile <- "gratitude.xlsx"
+curl::curl_download(url, destfile)
+gratitude <- read_excel(destfile)
+View(gratitude)
+
+# Numerical Summaries -------------------------------------
+library(mosaic)
+favstats(happiness ~ treatment, data = gratitude)
+
+# Checking ANOVA requirements -----------------------------
+var(happiness ~ treatment, data = gratitude)
+max( var(happiness ~ treatment, data = gratitude) ) # Max variance
+min( var(happiness ~ treatment, data = gratitude) ) # Min variance
+
+# Graphical Summaries -------------------------------------
+boxplot(happiness ~ treatment, data = gratitude)
+
+# Hypothesis Test -----------------------------------------
+gratitude_aov <- aov(happiness ~ treatment, data = gratitude)
+summary(gratitude_aov)
+
+
+
+# Soccer Shoes Example (Wide Data) ------------------------
+# Use "Import Dataset" to read the data into R
+# https://byuistats.github.io/M221R/data/soccer_shoes.xlsx
+# (You may want to paste the code here)
+library(readxl)
+url <- "https://byuistats.github.io/M221R/data/soccer_shoes.xlsx"
+destfile <- "soccer_shoes.xlsx"
+curl::curl_download(url, destfile)
+soccer_shoes <- read_excel(destfile)
+View(soccer_shoes)
+
+# Convert Data from Wide to Long Format -------------------
+soccer <- stack( Filter(is.numeric, soccer_shoes) )
+
+# Numerical Summaries -------------------------------------
+library(mosaic)
+favstats(values ~ ind, data = soccer)
+
+# Checking ANOVA requirements -----------------------------
+var(values ~ ind, data = soccer)
+max( var(values ~ ind, data = soccer) ) # Max variance
+min( var(values ~ ind, data = soccer) ) # Min variance
+
+# Graphical Summaries -------------------------------------
+boxplot(values ~ ind, data = soccer)
+
+# Hypothesis Test -----------------------------------------
+aov_output <- aov(values ~ ind, data = soccer)
+summary(aov_output)
---
+++ scripts/lesson13_next_semester.R
@@ -0,0 +1,85 @@
+# ===================================================================
+# Lesson 13: Inference for the Difference of Means (Two Independent Samples)
+# Sample R code
+# MATH 221D
+# Brigham Young University-Idaho
+# ===================================================================
+
+
+
+# Hypothesis Tests and Confidence Intervals (Long Data) =============
+
+
+
+# Reading Practices Example -------------------------------
+# Use "Import Dataset" to read the data into R
+# https://byuistats.github.io/M221R/data/copd_rehab.xlsx
+# (You may want to paste the code here)
+
+
+# Numerical Summaries -------------------------------------
+library(mosaic)
+favstats(nights ~ group, data = reading)
+
+# Graphical Summaries -------------------------------------
+boxplot(nights ~ group, data = reading)
+
+# Hypothesis Test -----------------------------------------
+t.test(nights ~ group, data = reading,
+       alternative = "two.sided")
+
+# Confidence Interval -------------------------------------
+t.test(nights ~ group, data = reading,
+       conf.level = 0.95)
+
+
+# FIFA Heart Attacks Example (Long Data) ------------------
+# Use "Import Dataset" to read the data into R
+# https://byuistats.github.io/M221R/data/fifa_heart_attacks.xlsx
+# (You may want to paste the code here)
+
+
+# Numerical Summaries -------------------------------------
+library(mosaic)
+favstats(heart_attacks ~ time_period, data = fifa_heart_attacks)
+
+# Graphical Summaries -------------------------------------
+boxplot(heart_attacks ~ time_period, data = fifa_heart_attacks)
+
+# Hypothesis Test -----------------------------------------
+t.test(heart_attacks ~ time_period, data = fifa_heart_attacks,
+       alternative = "less")
+
+# Confidence Interval -------------------------------------
+t.test(heart_attacks ~ time_period, data = fifa_heart_attacks,
+       conf.level = 0.95)
+
+
+
+# Hypothesis Tests and Confidence Intervals (Wide Data) =============
+
+
+
+# COPD Example (Wide Data) --------------------------------
+# Use "Import Dataset" to read the data into R
+# https://byuistats.github.io/M221R/data/copd_rehab.xlsx
+# (You may want to paste the code here)
+
+
+# Convert Data from Wide to Long Format (if necessary) ----
+copd <- stack( Filter(is.numeric, copd_rehab) )
+
+# Numerical Summaries -------------------------------------
+library(mosaic)
+favstats(values ~ ind, data = copd)
+
+# Graphical Summaries -------------------------------------
+boxplot(values ~ ind, data = copd)
+
+# Hypothesis Test -----------------------------------------
+t.test(values ~ ind, data = copd,
+       alternative = "two.sided")
+
+# Confidence Interval -------------------------------------
+t.test(values ~ ind, data = copd,
+       conf.level = 0.95)
